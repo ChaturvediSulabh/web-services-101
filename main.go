077f2a6c1f18d3b028bbb3d0e22f1dc1433fb6c7
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2020-06-14 01:40:51 +0100

    Added search handler

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 //Sample ...
@@ -36,17 +38,44 @@ type Topping []struct {
 	Type string `json:"type"`
 }
 
+//ToppingResponse ...
+type ToppingResponse struct {
+	Name string
+	Cost float64
+}
+
 func main() {
 	dataFile := flag.String("dataFile", "data/sample.json", "JSON file with sample data")
 	toppings := getToppings(*dataFile)
-	http.HandleFunc("/topping", toppingHandler(toppings))
+	http.HandleFunc("/topping", toppingsHandler(toppings))
+	http.HandleFunc("/toppings/", toppingHandler(*dataFile))
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func toppingHandler(toppings []Topping) func(http.ResponseWriter, *http.Request) {
+func toppingHandler(data string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		urlPathSegments := strings.Split(r.URL.Path, "/")
+		toppingType := urlPathSegments[len(urlPathSegments)-1]
+		myTopping, err := getToppingByName(toppingType, data)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		toppingsFound, err := json.Marshal(myTopping)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(toppingsFound)
+		w.Header().Set("Content-Type", "application/json")
+	}
+}
+
+func toppingsHandler(toppings []Topping) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "POST":
@@ -94,4 +123,36 @@ func getToppings(fileName string) []Topping {
 		toppings = append(toppings, s[i].Topping)
 	}
 	return toppings
+}
+
+func getToppingByName(toppingType string, fileName string) ([]ToppingResponse, error) {
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var s Sample
+	err = json.Unmarshal(data, &s)
+	if err != nil {
+		return nil, err
+	}
+	var r []ToppingResponse
+	doesExists := false
+	for _, st := range s {
+		for _, t := range st.Topping {
+			if toppingType == t.Type {
+				if doesExists == false {
+					doesExists = true
+				}
+				data := ToppingResponse{
+					st.Name,
+					st.Ppu,
+				}
+				r = append(r, data)
+			}
+		}
+		if doesExists == false {
+			return nil, errors.New("Topping Not Found")
+		}
+	}
+	return r, nil
 }

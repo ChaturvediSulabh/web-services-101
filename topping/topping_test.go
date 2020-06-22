6e12e80a509359e8956bc9f19c470b7d64cb1073
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2020-06-22 22:46:21 +0100

    Add topping tests

--- topping/topping_test.go
+++ topping/topping_test.go
@@ -1,14 +1,31 @@
 package topping
 
-import "testing"
+import (
+	"testing"
+
+	_ "github.com/lib/pq"
+)
 
 func TestFetchMenu(t *testing.T) {
-	// fetchMenu()
+	_, err := fetchMenu()
+	if err != nil {
+		t.Error(err)
+	}
 }
 func TestGetToppings(t *testing.T) {
-	//getToppings()
+	_, err := getToppings()
+	if err != nil {
+		t.Error(err)
+	}
 }
 
 func TestGetToppingByName(t *testing.T) {
-	// getToppingByName(toppingType string)
+	toppingType := [...]string{"None", "Powdered Sugar"}
+	for _, str := range toppingType {
+		result, err := getToppingByName(str)
+		if err != nil {
+			t.Error(err)
+		}
+		t.Logf("%+v\n", result)
+	}
 }
